04e65e00d827ecbc2ed7be2cdc9166767a012e3d
Paul K Lynch <dramatichamster@gmail.com>
2020-12-08 17:33:45 +1100

    Swapping to suggestions panel

--- src/TimeRange/component/TimeForm.jsx
+++ src/TimeRange/component/TimeForm.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { zeroPadUnit, timeToFloat } from '../helpers/format';
+// import classNames from 'classnames';
+import { zeroPadUnit } from '../helpers/format';
 
 const TimeForm = ({ isOpen, startTime, timeValue, suggestTimes, setTimeValue }) => {
   const [ defaultHhValue, defaultMmValue ] = timeValue.split(':');
-  // console.log('test', hh, mm);
-  const [ activeTab, setActivetab ] = useState('hh');
+  // const [ activeTab, setActivetab ] = useState('hh');
   const [ hhValue, setHhValue ] = useState(defaultHhValue);
   const [ mmValue, setMmValue ] = useState(defaultMmValue);
   const inputHhEl = useRef(null); // ref for hours input
@@ -20,6 +19,12 @@ const TimeForm = ({ isOpen, startTime, timeValue, suggestTimes, setTimeValue }) 
       }, 100)
     }
   }, [isOpen]);
+
+  useEffect(() => {
+    const [ newHhValue = '', newMmValue = '' ] = timeValue.split(':');
+    setHhValue(newHhValue);
+    setMmValue(newMmValue)
+  }, [ timeValue, setHhValue, setMmValue ]);
 
   // On value update: update total time value 
   useEffect(() => {
@@ -53,7 +58,7 @@ const TimeForm = ({ isOpen, startTime, timeValue, suggestTimes, setTimeValue }) 
 
   // On Hours input focus
   const onHhFocus = ({ target }) => {
-    setActivetab('hh');
+    // setActivetab('hh');
     target.select();
   }
 
@@ -99,60 +104,15 @@ const TimeForm = ({ isOpen, startTime, timeValue, suggestTimes, setTimeValue }) 
 
   // On Minutes input focus
   const onMmFocus = ({ target }) => {
-    setActivetab('mm');
+    // setActivetab('mm');
+
     target.select();
   }
 
-  const onHhBtnClick = ({ target: { value } }) => {
-    setHhValue(value);
-    inputMmEl.current.focus();
+  const onSuggestClick = ({ target: { value } }) => {
+    console.log('click', value);
+    setTimeValue(value); // FIX
   }
-
-  const onMmBtnClick = ({ target: { value } }) => {
-    setMmValue(value);
-    // TODO to something after
-  }
-
-  console.log('suggestTimes', suggestTimes);
-
-  const hhValueBtns = [];
-  for (let hr = 0; hr <= 23; hr++) {
-    const zeroPadHr = zeroPadUnit(hr);
-    const timeAsFlaot = timeToFloat(`${zeroPadHr}:00`);
-    
-
-    // console.log('float', `${zeroPadHr}:${mmValue || '00'}`, timeToFloat(`${zeroPadHr}:${mmValue || '00'}`) > timeToFloat(startTime));
-
-    if (timeAsFlaot > timeToFloat(startTime)) {
-      hhValueBtns.push(
-        <li className="time-form__time-item" key={`hh-val-item-${zeroPadHr}`}>
-          <button 
-            type="button"
-            className="time-form__value-btn"
-            value={zeroPadHr}
-            onClick={onHhBtnClick}
-          ><strong>{zeroPadHr}</strong>:{mmValue || '00'}</button>
-        </li>
-      );
-    }
-  }
-
-  const mmValueBtns = [];
-  for (let min = 0; min <= 45; min += 15) {
-    let zeroPadMin = zeroPadUnit(min);
-    mmValueBtns.push(
-      <li className="time-form__time-item" key={`hh-val-item-${zeroPadMin}`} >
-        <button 
-          type="button"
-          className="time-form__value-btn"
-          value={zeroPadMin}
-          onClick={onMmBtnClick}
-        >{hhValue || '00'}:<strong>{zeroPadMin}</strong></button>
-      </li>
-    );
-  }
-
-  const tabClasses = tabType => classNames('time-form__tab', { 'time-form__tab--active': (tabType === activeTab ) })
 
   if (!isOpen) {
     return null;
@@ -160,57 +120,50 @@ const TimeForm = ({ isOpen, startTime, timeValue, suggestTimes, setTimeValue }) 
 
   return (
     <section className="time-form">
-      <ul role="tablist" className="time-form__tabs" aria-label="Time Form">
-        <li className={tabClasses('hh')}>
-          <input 
-            type="text"
-            className="time-form__input"
-            value={hhValue}
-            ref={inputHhEl}
-            pattern="[0-9]*"
-            inputMode="numeric"
-            onChange={onHhChange}
-            onFocus={onHhFocus}
-            onBlur={onHhBlur}
-          />
-        </li>
-        <li className="time-form__semi">:</li>
-        <li className={tabClasses('mm')}>
-          <input
-            type="text"
-            className="time-form__input"
-            value={mmValue}
-            ref={inputMmEl}
-            pattern="[0-9]*"
-            inputMode="numeric"
-            onChange={onMmChange}
-            onFocus={onMmFocus}
-            onBlur={onMmBlur}
-          />
-        </li>
-      </ul>
 
-      <div className={classNames('time-form__panel', { 'time-form__panel--active': (activeTab !== 'xxxxhh') })}>
-        <ul className="time-form__times-list">
-          <li className="time-form__time-item">
-            <button 
-              type="button"
-              className="time-form__value-btn"
-              value={'09:30'}
-              onClick={() => {}}
-            >09:30</button>
-          </li>
+      <div className="time-form__fields">
+        <input 
+          type="text"
+          className="time-form__input"
+          value={hhValue}
+          ref={inputHhEl}
+          pattern="[0-9]*"
+          inputMode="numeric"
+          onChange={onHhChange}
+          onFocus={onHhFocus}
+          onBlur={onHhBlur}
+        />
+        <span className="time-form__semi">:</span>
+        <input
+          type="text"
+          className="time-form__input"
+          value={mmValue}
+          ref={inputMmEl}
+          pattern="[0-9]*"
+          inputMode="numeric"
+          onChange={onMmChange}
+          onFocus={onMmFocus}
+          onBlur={onMmBlur}
+        />
+      </div>
+
+      <div className="suggestions">
+        <h2 className="suggestions__title">Suggestions</h2>
+
+        <ul className="suggestions__list">
+          {suggestTimes.filter(time => true /* TODO: fitler by starting time */).map(time => (
+            <li key={`time-${time}`} className="suggestions__item">
+              <button 
+                className="suggestions__time-btn"
+                value={time}
+                onClick={onSuggestClick}
+              >{time}</button>
+            </li>
+          ))}
         </ul>
+
       </div>
 
-      <div className={classNames('time-form__panel', { 'time-form__panel--active': (activeTab === 'xhh') })}>
-        <ul className="time-form__times-list">{hhValueBtns}</ul>
-      </div>
-
-      <div className={classNames('time-form__panel', { 'time-form__panel--active': (activeTab === 'xmm') })}>
-      <ul className="time-form__times-list">{mmValueBtns}</ul>
-      </div>
-      
     </section>
   );
 }
